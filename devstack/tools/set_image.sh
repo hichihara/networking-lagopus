c08a4418a7fba8c31f4ad5f5060edcdf30d7c08e
Hirofumi Ichihara <ichihara.hirofumi@gmail.com>
2016-11-05 18:17:15 +0000

    Fix image link in tools

--- devstack/tools/set_image.sh
+++ devstack/tools/set_image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget "http://templateimage.str.cloudn-service.com/ubuntu1404.raw?AWSAccessKeyId=I5M4F3PLSDGWHR8RSETY&Expires=1509815820&Signature=7iS8sNnuK%2BmAdMv0l%2FWGociasqc%3D&x-amz-pt=NGI1MTQyOTI3NjE0NzgyNzYzMDI4MTQ" -O ubuntu1404.raw
+wget "http://templateimage.str.cloudn-service.com/ubuntu1404-tso-off.qcow2?AWSAccessKeyId=I5M4F3PLSDGWHR8RSETY&Expires=1509909060&Signature=2d0HGRgK%2FzBur80j9dY1x8pOs0c%3D&x-amz-pt=NGQ4ZWVjOTI3NjE0NzgzNjk3OTA4Nzg" -O ubuntu1404.raw
 openstack --os-username admin --os-password secret image create ubuntu --disk-format qcow2 --container-format bare --public --file ./ubuntu1404.raw
 nova --os-username admin --os-password secret flavor-create test 99 1024 3 1
